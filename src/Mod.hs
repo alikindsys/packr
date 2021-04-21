2dce8870b23a38af9b763956f5510ecd42147a62
roridev <t3ctotalmenterandom1@outlook.com>
2021-04-21 16:04:38 -0300

    Adds FabricJson format

--- src/Mod.hs
+++ src/Mod.hs
@@ -2,3 +2,26 @@ module Mod (
 
     )
      where
+
+
+data FabricJson = FabricJson
+    {   schemaVersion :: Int 
+    ,   id :: String
+    ,   _version :: String
+    ,   __description :: String
+    ,   authors :: ![Either String FabricPerson]
+    ,   contact :: !FabricContact 
+    }
+
+data FabricContact = FabricContact
+    {   homepage :: !String
+    ,   sources :: !String
+    ,   issues :: !String
+    ,   irc :: !String
+    ,   email :: !String
+    }
+
+data FabricPerson = FabricPerson
+    {   _name :: String
+    ,   _contact :: !FabricContact
+    }
